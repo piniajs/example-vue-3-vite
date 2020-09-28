64fa4ee37ad0a59f0c37df17083da4530f78a07c
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-28 17:21:11 +0200

    should fail [skip ci]

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import { pinia } from './stores'
+// make it fail
+import { pinia } from './stores'
 
 const app = createApp(App)
 
-// app.use(pinia)
+app.use(pinia)
 
 app.mount('#app')
