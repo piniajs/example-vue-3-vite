b43f0ce517ac87ff44ffccb19130124975bc5929
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-28 16:46:06 +0200

    fix: unused version yet

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { pinia } from './stores'
+// import { pinia } from './stores'
 
-const app = createApp(App).use(pinia)
+const app = createApp(App)
+
+// app.use(pinia)
 
 app.mount('#app')
